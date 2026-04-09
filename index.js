c449104bc93ff30e0f6df12d750bb28bf6e83ee8
Deepak <deepakkushwaha5945@gmail.com>
2026-04-10 03:09:25 +0530

    updated code in MongoDb connections

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ require('dotenv').config()
 
 //To allow the data from fronted to backend
 App.use(express.json())
- 
+
 
 // To manages the diffrents ports like fronted port in backend 
-let cors=require('cors')
+let cors = require('cors')
 App.use(cors())
 
 
-App.use('/admin',adminRoute)
-App.use("/portfolio-API",portfolioRoutes)
+App.use('/admin', adminRoute)
+App.use("/portfolio-API", portfolioRoutes)
 
 
 // To Allow to show img url to access any where..
 
-App.use('/uploads/profile',express.static('uploads/profile'))
-App.use('/uploads/resume',express.static('uploads/resume'))
-App.use('/uploads/internship',express.static('uploads/internship'))
-App.use('/uploads/project',express.static('uploads/project'))
-App.use('/uploads/certificate',express.static('uploads/certificate'))
+App.use('/uploads/profile', express.static('uploads/profile'))
+App.use('/uploads/resume', express.static('uploads/resume'))
+App.use('/uploads/internship', express.static('uploads/internship'))
+App.use('/uploads/project', express.static('uploads/project'))
+App.use('/uploads/certificate', express.static('uploads/certificate'))
 
 
 
@@ -39,11 +39,20 @@ App.get('/', (req, res) => {
 })
 
 
+// mongoose.connect(process.env.DbUrl)
+//     .then(() => {
+//         App.listen(4000, () => {
+//             console.log("server start...", 4000);
+//         })
+//     })
 mongoose.connect(process.env.DbUrl)
     .then(() => {
-        App.listen(4000, () => {
-            console.log("server start...", 4000);
+        // http://localhost:9000
+        App.listen(process.env.PORT, () => {
+            console.log('DB connected to ATLAS..', process.env.PORT,);
         })
+
+
     })
     .catch((error) => {
         console.log(error);
