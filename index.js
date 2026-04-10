b1c1968da5e6a6e98e0ad49051a57e04c75e8009
Deepak <deepakkushwaha5945@gmail.com>
2026-04-11 02:51:25 +0530

    updated inde.js

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ let App = express();
 require('dotenv').config();
 
 App.use(express.json());
+App.use(express.urlencoded({ extended: true }));
 App.use(cors());
 
-// 🔥 DB connect once
+// DB connect
 mongoose.connect(process.env.DbUrl)
     .then(() => console.log("DB connected to ATLAS.."))
-    .catch(err => console.log(err));
+    .catch(err => console.log("DB Error:", err));
 
 // Routes
 App.use('/admin', adminRoute);
@@ -23,6 +24,12 @@ App.use("/portfolio-API", portfolioRoutes);
 // Home
 App.get('/', (req, res) => {
     res.send({ status: true, message: "Backend is working now...." });
+});
+
+// Global Error Handler
+App.use((err, req, res, next) => {
+    console.error("Global Error:", err);
+    res.status(500).json({ status: false, message: err.message || "Internal Server Error" });
 });
 
 module.exports = App;
