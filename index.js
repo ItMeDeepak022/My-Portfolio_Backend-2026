9b64bacb619b3b34c8f16c04bb0fc0593c44492e
Deepak <deepakkushwaha5945@gmail.com>
2026-04-10 18:04:07 +0530

    updated code

--- index.js
+++ index.js
@@ -1,73 +1,30 @@
-// let express=require('express')
-// let mongoose = require('mongoose')
-// const { adminRoute } = require('./backend/routers/adminRoute')
-// const { portfolioRoutes } = require('./backend/routers/portfolioRoute')
-// let App = express()
-
-// // To access the .env variables
-// require('dotenv').config()
-
-// //To allow the data from fronted to backend
-// App.use(express.json())
-
-
-// // To manages the diffrents ports like fronted port in backend 
-// let cors = require('cors')
-// App.use(cors())
-
-
-// App.use('/admin', adminRoute)
-// App.use("/portfolio-API", portfolioRoutes)
-
-
-// // To Allow to show img url to access any where..
-
-// // App.use('/uploads/profile', express.static('uploads/profile'))
-// // App.use('/uploads/resume', express.static('uploads/resume'))
-// // App.use('/uploads/internship', express.static('uploads/internship'))
-// // App.use('/uploads/project', express.static('uploads/project'))
-// // App.use('/uploads/certificate', express.static('uploads/certificate'))
-
-// // Also use it for view photo as live after hosted your backend
-
-// App.use('/uploads/profile', express.static('/tmp/uploads/profile'))
-// App.use('/uploads/resume', express.static('/tmp/uploads/resume'))
-// App.use('/uploads/internship', express.static('/tmp/uploads/internship'))
-// App.use('/uploads/project', express.static('/tmp/uploads/project'))
-// App.use('/uploads/certificate', express.static('/tmp/uploads/certificate'))
-
-// //https://localhost:9000
-// App.get('/', (req, res) => {
-//     res.send({
-//         status: true,
-//         message: "Backend is working now...."
-//     })
-// })
-
-
-//  mongoose.connect(process.env.DbUrl)
-//     .then(() => {
-//         console.log('DB connected to ATLAS..');
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-
-// module.exports = App
-
-
-
 let express = require('express')
 let mongoose = require('mongoose')
 const { adminRoute } = require('./backend/routers/adminRoute')
 const { portfolioRoutes } = require('./backend/routers/portfolioRoute')
 let cors = require('cors')
+let fs = require('fs');
 let App = express()
 
 require('dotenv').config()
 
 App.use(express.json())
 App.use(cors())
+
+
+const ensureDir = (dir) => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+};
+
+// create all folders in /tmp
+ensureDir("/tmp/uploads/profile");
+ensureDir("/tmp/uploads/resume");
+ensureDir("/tmp/uploads/internship");
+ensureDir("/tmp/uploads/project");
+ensureDir("/tmp/uploads/certificate");
+
 
 // DB connection with caching
 let isConnected = false
